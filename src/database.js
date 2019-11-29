3a4adddbb63f18adda8d24aff20e704b55a5b561
Desarrollo <jose.sevilla@gme.mx>
2019-11-28 19:03:33 -0600

    Añadimos bootstrap e implementamos formulario añadir notas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.createConnection('mongodb+srv://<<user>>:<<password>>@cluster0-zrsf9.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.createConnection('mongodb+srv://<user>:<password>@cluster0-zrsf9.mongodb.net/test?retryWrites=true&w=majority',{
     useCreateIndex : true,
     useNewUrlParser :true,
     useUnifiedTopology : true,
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add',(req,res)=>{
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note',(req,res)=>{
+    const { title , description } = req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text: 'please Write a Title'});
+    }
+    if(!description){
+        errors.push({text:'Please Write a Description'});
+    }
+    if(errors.length >0){
+        res.render('notes/new-note',{ 
+            errors,
+            title,
+            description
+        });
+    }else{
+        res.send('ok');
+    }
+});
+
 router.get('/notes',(req,res)=>{
     res.send('Notes from database');
 });
